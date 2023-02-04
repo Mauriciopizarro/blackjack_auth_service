49c9e28bad15838ae2885e4dfad6cb852a4cb4dd
Mauri <pizarromauricio031096@gmail.com>
2023-02-04 13:16:26 -0300

    Improve handling errors in auth

--- src/main/java/com/devdream/blackjackaccountservice/infrastructure/security/UserDetailServiceImplementation.java
+++ src/main/java/com/devdream/blackjackaccountservice/infrastructure/security/UserDetailServiceImplementation.java
@@ -3,7 +3,6 @@ package com.devdream.blackjackaccountservice.infrastructure.security;
 import com.devdream.blackjackaccountservice.application.implementations.UserAccountRepo;
 import com.devdream.blackjackaccountservice.domain.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
