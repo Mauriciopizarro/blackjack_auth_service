b27d026671f5e89edd3b0c45761478e4cf51ac79
Mauricio Pizarro <pizarromauricio031096@gmail.com>
2024-02-21 19:38:53 -0300

    sending-json-body-to-rabbitmq (#9)

--- src/main/java/com/devdream/blackjackaccountservice/BlackjackAccountServiceApplication.java
+++ src/main/java/com/devdream/blackjackaccountservice/BlackjackAccountServiceApplication.java
@@ -2,23 +2,14 @@ package com.devdream.blackjackaccountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.devdream.blackjackaccountservice", "com.devdream.blackjackaccountservice.infrastructure.publisher"})
 public class BlackjackAccountServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlackjackAccountServiceApplication.class, args);
 	}
 
-	@Bean
-	public HealthIndicator customHealthIndicator() {
-		return () -> Health.up().withDetail("message", "Service is running").build();
-	}
 
 }
